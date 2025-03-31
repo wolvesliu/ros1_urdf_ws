72dcb1d3bba169929231cdf64d578af0101b46b9
max <wolves_liu@163.com>
2025-03-31 01:17:41 -0700

    feat: 包含赵虚左讲的urdf、rviz、gazebo、导航

---
+++ ros1_urdf_ws/build/nav_demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/song/ros1_urdf_ws/src/nav_demo
+# Build directory: /home/song/ros1_urdf_ws/build/nav_demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ros1_urdf_ws/build/urdf01_rviz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/song/ros1_urdf_ws/src/urdf01_rviz
+# Build directory: /home/song/ros1_urdf_ws/build/urdf01_rviz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
