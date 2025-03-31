0ace89a7464ca9d8fd9e788c3a376d5648ba8559
max <wolves_liu@163.com>
2025-03-31 01:08:25 -0700

    feat: 初始化 ROS 服务包 learning_service

---
+++ learning_service/src/demo02_client.cpp
@@ -0,0 +1,69 @@
+/*
+    需求: 
+        编写两个节点实现服务通信，客户端节点需要提交两个整数到服务器
+        服务器需要解析客户端提交的数据，相加后，将结果响应回客户端，
+        客户端再解析
+
+        格式：rorun xxx xxx 12  34
+
+    服务器实现:
+        1.包含头文件
+        2.初始化 ROS 节点
+        3.创建 ROS 句柄
+        4.创建 客户端 对象
+        5.请求服务，接收响应
+
+*/
+// 1.包含头文件
+#include "ros/ros.h"
+#include "learning_service/AddInts.h"
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL,"");
+
+    // 调用时动态传值,如果通过 launch 的 args 传参，需要传递的参数个数 +3
+    if (argc != 3)
+    // if (argc != 5)//launch 传参(0-文件路径 1传入的参数 2传入的参数 3节点名称 4日志路径)
+    {
+        ROS_ERROR("请提交两个整数");
+        return 1;
+    }
+
+
+    // 2.初始化 ROS 节点
+    ros::init(argc,argv,"demo02_client");
+    // 3.创建 ROS 句柄
+    ros::NodeHandle nh;
+    // 4.创建 客户端 对象
+    ros::ServiceClient client = nh.serviceClient<learning_service::AddInts>("AddInts");//<数据类型>(服务名）
+    //等待服务启动成功
+    //方式1
+    ros::service::waitForService("AddInts");
+
+    //方式2
+    // client.waitForExistence();
+    // 5.组织请求数据
+    learning_service::AddInts ai; //创建数据对象,数据类型：功能包（learning_service）下的AddInts
+    ai.request.num1 = 100;
+    ai.request.num2 = 200;
+
+
+    ai.request.num1 = atoi(argv[1]);//int
+    ai.request.num2 = atoi(argv[2]);
+
+    // 6.发送请求,返回 bool 值，标记是否成功
+    bool flag = client.call(ai);
+    // 7.处理响应
+    if (flag)
+    {
+        ROS_INFO("请求正常处理,响应结果:%d",ai.response.sum);
+    }
+    else
+    {
+        ROS_ERROR("请求处理失败....");
+        return 1;
+    }
+
+    return 0;
+}
